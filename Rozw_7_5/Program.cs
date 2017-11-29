8101f5ae5f3cb1cff661ed84bf25eb2311418277
Piotrek <@>
2017-11-29 13:30:18 +0100

    przeslanie plików

---
+++ Rozw_7_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_7_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime teraz = DateTime.Now;
+
+            Console.WriteLine(teraz.Day.ToString().PadLeft(2, '0') + teraz.Month.ToString().PadLeft(2, '0') + teraz.Year.ToString().PadLeft(4, '0') + teraz.Hour.ToString().PadLeft(2, '0') + teraz.Minute.ToString().PadLeft(2, '0') + teraz.Second.ToString().PadLeft(2, '0'));
+
+            Console.ReadKey();
+        }
+    }
+}
